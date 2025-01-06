9303e3259f0fa3f8db99ed08b67ee37355c68c7b
theDevanshShah <devansh.shah.de@gmail.com>
2025-01-06 10:16:52 +0530

    Variables & Input Output

--- first.py
+++ first.py
@@ -1,1 +1,30 @@
+# first.py 
 print("Hello World")
+
+# Defining Variables
+name = "Devansh Shah"
+age = 20
+height = 5.9
+nameStartsWith = "D"
+isStudent = True
+
+# Printing Variables
+print(name)
+print(age)
+print(height)
+print(nameStartsWith)
+print(isStudent)
+
+# Taking Input
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+height = input("Enter your height: ")
+nameStartsWith = input("Enter the first letter of your name: ")
+isStudent = input("Are you a student? ")
+
+# Again Printing Variables
+print(name)
+print(age)
+print(height)
+print(nameStartsWith)
+print(isStudent)
